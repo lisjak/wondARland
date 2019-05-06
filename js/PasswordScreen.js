343342f008e3f71925c5bb4ab97009901fd8a1c8
Xiaoyu Zhu <emmazxy@gmail.com>
2019-05-06 11:23:52 -0400

    commit before merge origin master

--- js/PasswordScreen.js
+++ js/PasswordScreen.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { View,Text, StyleSheet } from 'react-native';
 import { Input, Button } from 'react-native-elements'
-export default class PasswordScreen extends Component{
 
+export default class PasswordScreen extends Component{
   constructor(){
     super();
     this.state = {
