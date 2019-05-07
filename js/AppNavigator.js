fcfb20b1766f11f23a8be40b8697fd9c8e310d4d
Lisa Jakubczyk <lisajakubczyk@gmail.com>
2019-05-07 16:11:14 -0400

    refactoring yet again

--- js/AppNavigator.js
+++
@@ -1,9 +0,0 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-
-import Loser from './Loser';
-
-const AppNavigator = createStackNavigator({
-  Loser: { screen: Loser },
-});
-
-export default createAppContainer(AppNavigator);
