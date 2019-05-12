79ae74e125026116369c1c3c5d6b9dcd3303f80e
Reina Murooka <reinamurooka@gmail.com>
2019-05-11 23:55:36 -0400

    added new portal doors, new dead-end portalscene prototype with 3d text, music added to cheshire cat scene

---
+++ ARScenes/Portals/DeadEndforRosePortal.js
@@ -0,0 +1,139 @@
+import React, { Component } from '../../node_modules/react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import {
+  Viro3DObject,
+  ViroSound,
+  ViroNode,
+  Viro360Image,
+  ViroPortal,
+  ViroPortalScene,
+  ViroText,
+  ViroMaterials,
+} from '../../node_modules/react-viro';
+
+import RoseObject from './RoseObject';
+
+export default class DeadEndforRosePortal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playPortal1Sound: true,
+      showText: false,
+    };
+    this.handleEnterPortal1 = this.handleEnterPortal1.bind(this);
+    this.handleExitPortal1 = this.handleExitPortal1.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleEnterPortal1() {
+    this.setState({ playPortal1Sound: false });
+  }
+
+  handleExitPortal1() {
+    this.setState({ playPortal1Sound: true });
+  }
+  handleClick() {
+    this.setState({ showText: true });
+  }
+
+  render() {
+    return (
+      <ViroPortalScene
+        position={[0, 0, -2]}
+        passable={true}
+        onPortalEnter={this.handleEnterPortal1}
+        onPortalExit={this.handleExitPortal1}
+      >
+        <ViroPortal position={[0, 0, -2]} scale={[0.5, 0.5, 0.5]}>
+          <Viro3DObject
+            source={require('../../assets/portal_assets/portal_res/portal_wood_frame/portal_wood_frame.vrx')}
+            resources={[
+              require('../../assets/portal_assets/portal_res/portal_wood_frame/portal_wood_frame_diffuse.png'),
+              require('../../assets/portal_assets/portal_res/portal_wood_frame/portal_wood_frame_normal.png'),
+              require('../../assets/portal_assets/portal_res/portal_wood_frame/portal_wood_frame_specular.png'),
+            ]}
+            type="VRX"
+          />
+        </ViroPortal>
+        <Viro360Image
+          source={require('../../assets/portal_assets/360maze.jpg')}
+        />
+        <ViroNode position={[1, 1, -1]}>
+          <ViroText
+            text="Oops, this is a dead end!"
+            // width={2}
+            height={5}
+            scale={[0.5, 0.5, 0.5]}
+            extrusionDepth={8}
+            materials={['frontMaterial', 'backMaterial', 'sideMaterial']}
+            position={[0, 1, -2]}
+            style={styles.boldFont}
+            visible={this.state.showText}
+          />
+
+          <Viro3DObject
+            source={require('../../assets/emoji_heart/emoji_heart.vrx')}
+            resources={[
+              require('../../assets/emoji_heart/emoji_heart_specular.png'),
+              require('../../assets/emoji_heart/emoji_heart.png'),
+            ]}
+            position={[0, 0, -1]}
+            scale={[0.3, 0.3, 0.3]}
+            onClick={this.handleClick}
+            type="VRX"
+          />
+        </ViroNode>
+        {/* <HeartObject position={[1, 1, -3]} />
+        <HeartObject position={[0, 2, -5]} />
+        <HeartObject position={[2, -1, -4]} />
+        <HeartObject position={[0, 4, -1]} /> */}
+        <RoseObject position={[-3, 2, -5]} scale={[0.02, 0.02, 0.02]} />
+
+        <ViroSound
+          paused={this.state.playPortal1Sound}
+          muted={false}
+          source={require('../../assets/music/comfortable-mystery-by-kevin-macleod.mp3')}
+          loop={true}
+          volume={1.0}
+        />
+      </ViroPortalScene>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#C8243B',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+  boldFont: {
+    color: '#FFFFFF',
+    flex: 1,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  portalTextStyles: {
+    fontFamily: 'Arial',
+    fontSize: 28,
+    color: '#C8243B',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
+
+ViroMaterials.createMaterials({
+  frontMaterial: {
+    diffuseColor: '#FFFFFF',
+  },
+  backMaterial: {
+    diffuseColor: '#008080',
+  },
+  sideMaterial: {
+    diffuseColor: '#cd5c5c',
+  },
+});
--- ARScenes/Portals/QueenPortal.js
+++ ARScenes/Portals/QueenPortal.js
@@ -13,9 +13,9 @@ import {
   ViroAnimatedImage,
   ViroPortal,
   ViroPortalScene,
-  Viro3DObject
+  Viro3DObject,
+  ViroSound,
 } from '../../node_modules/react-viro';
-
 
 const cheshireCat = require('../../assets/portal_assets/cheshirecatsmile.png');
 
@@ -26,52 +26,98 @@ export default class MainScene extends Component {
     // set initial state
     this.state = {
       runShowTitleAnimation: true,
+      playPortal1Sound: true,
+      showText: false,
     };
 
     // bind `this` to functions
     this._onBackgroundPhotoLoadEnd = this._onBackgroundPhotoLoadEnd.bind(this);
+    this.handleEnterPortal1 = this.handleEnterPortal1.bind(this);
+    this.handleExitPortal1 = this.handleExitPortal1.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     // this._onTitleClicked = this._onTitleClicked.bind(this);
   }
 
+  handleClick() {
+    this.setState({ showText: true });
+  }
+  handleEnterPortal1() {
+    this.setState({ playPortal1Sound: false });
+  }
+
+  handleExitPortal1() {
+    this.setState({ playPortal1Sound: true });
+  }
   render() {
     return (
       <ViroPortalScene
         position={[0, 1, -1]}
         passable={true}
+        onPortalEnter={this.handleEnterPortal1}
+        onPortalExit={this.handleExitPortal1}
       >
-      <ViroPortal position={[0, 0, -2]} scale={[0.5, 0.5, 0.5]}>
-        <Viro3DObject
-          source={require('../../assets/portal_assets/portal_res/portal_ship/portal_ship.vrx')}
-          resources={[
-            require('../../assets/portal_assets/portal_res/portal_ship/portal_ship_diffuse.png'),
-            require('../../assets/portal_assets/portal_res/portal_ship/portal_ship_normal.png'),
-            require('../../assets/portal_assets/portal_res/portal_ship/portal_ship_specular.png'),
-          ]}
-          type="VRX"
+        <ViroPortal position={[0, 1, -1]} scale={[0.5, 0.5, 0.5]}>
+          <Viro3DObject
+            source={require('../../assets/portal_assets/portal_res/portal_ship/portal_ship.vrx')}
+            resources={[
+              require('../../assets/portal_assets/portal_res/portal_ship/portal_ship_diffuse.png'),
+              require('../../assets/portal_assets/portal_res/portal_ship/portal_ship_normal.png'),
+              require('../../assets/portal_assets/portal_res/portal_ship/portal_ship_specular.png'),
+            ]}
+            type="VRX"
+          />
+        </ViroPortal>
+
+        <ViroAnimatedImage
+          position={[0, 1, -10]}
+          // source={cheshireCat}
+          scale={[30, 30, 30]}
+          // opacity={0.0}
+          // onClick={this._onTitleClicked}
+          animation={{
+            name: 'showTitleAnimation',
+            run: true,
+            loop: true,
+            delay: 0,
+          }}
+          height={2}
+          width={2}
+          source={cheshireCat}
         />
-
-      </ViroPortal>
-
-      <ViroAnimatedImage
-        position={[0, 1, -10]}
-        // source={cheshireCat}
-        scale={[30, 30, 30]}
-        // opacity={0.0}
-        // onClick={this._onTitleClicked}
-        animation={{
-          name: 'showTitleAnimation',
-          run: true,
-          loop: true,
-          delay: 0
-        }}
-        height={2}
-        width={2}
-        source={cheshireCat}
-      />
 
         <Viro360Image
           // onLoadEnd={this._onBackgroundPhotoLoadEnd}
           source={require('../../assets/portal_assets/allblack.jpg')}
+        />
+        <ViroNode position={[1, 1, -1]}>
+          <ViroText
+            text="Oops, this is a dead end!"
+            width={2}
+            height={2}
+            scale={[0.5, 0.5, 0.5]}
+            position={[0, 1, -2]}
+            style={styles.portalTextStyles}
+            visible={this.state.showText}
+          />
+
+          <Viro3DObject
+            source={require('../../assets/emoji_heart/emoji_heart.vrx')}
+            resources={[
+              require('../../assets/emoji_heart/emoji_heart_specular.png'),
+              require('../../assets/emoji_heart/emoji_heart.png'),
+            ]}
+            position={[0, 0, -1]}
+            scale={[0.3, 0.3, 0.3]}
+            onClick={this.handleClick}
+            type="VRX"
+          />
+        </ViroNode>
+        <ViroSound
+          paused={this.state.playPortal1Sound}
+          muted={false}
+          source={require('../../aassets/music/giant-wyrm-by-kevin-macleod.mp3')}
+          loop={true}
+          volume={1.0}
         />
       </ViroPortalScene>
     );
@@ -84,12 +130,18 @@ export default class MainScene extends Component {
   }
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  portalTextStyles: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#C8243B',
+    textAlignVertical: 'center',
+    textAlign: 'center',
   },
 });
 
